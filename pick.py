659a9fe8f616c522dd9c8a86c7255f555a789b74
abdullahi-7 <abdullahi.abdi@protonmail.com>
2021-08-21 12:44:31 +0300

    pick project

---
+++ pick.py
@@ -0,0 +1,12 @@
+import time
+
+print("")
+
+play = int(input("pick a number?"))
+playing = 2
+
+pick = play + playing
+playjack = pick - playing
+
+print("plese you are done wait 5....seconds")
+time.sleep(5)
